c40c274c40962e173ceee7fefc77981ca821f000
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 12:03:25 +0530

    Added: Linked List

--- src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
+++ src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
@@ -1,9 +1,15 @@
 package com.bridgelabz.practiceproblem.linkedlist;
 
+import java.util.LinkedList;
+
 public class MainLinkedList {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to linked list program");
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		System.out.println(list);
 	}
 
 }
