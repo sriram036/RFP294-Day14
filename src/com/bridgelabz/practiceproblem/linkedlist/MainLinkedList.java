f3d01aa742098f427f11732aeb3de3b87023953d
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 12:09:43 +0530

    Added: Linked List AddFirst Method

--- src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
+++ src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
@@ -6,9 +6,9 @@ public class MainLinkedList {
 	
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<>();
-		list.add(56);
+		list.addFirst(70);
 		list.add(30);
-		list.add(70);
+		list.addLast(56);
 		System.out.println(list);
 	}
 
