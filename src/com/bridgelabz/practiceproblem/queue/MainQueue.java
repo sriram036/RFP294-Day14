4f4ebb8282039d5d8502e1165a923cdcb1f02011
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 18:13:45 +0530

    Added: Enque Method

---
+++ src/com/bridgelabz/practiceproblem/queue/MainQueue.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.practiceproblem.queue;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MainQueue {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.addLast(56);
+		list.addLast(30);
+		list.addLast(70);
+		System.out.println(list);
+		System.out.println("Top of the list is " + list.peek());
+	}
+}
