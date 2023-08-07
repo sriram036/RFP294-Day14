358a74c168eea26b21f4defd0be1163a2fffbdb7
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 12:14:50 +0530

    Added: Append Method

--- src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
+++ src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
@@ -6,9 +6,9 @@ public class MainLinkedList {
 	
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<>();
-		list.addFirst(70);
-		list.add(30);
 		list.addLast(56);
+		list.addLast(30);
+		list.addLast(70);
 		System.out.println(list);
 	}
 
