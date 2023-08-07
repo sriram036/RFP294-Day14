4b1c9a8500090fc804edabd8fc356a19e166f841
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 11:53:59 +0530

    First Commit

---
+++ src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.practiceproblem.linkedlist;
+
+public class MainLinkedList {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to linked list program");
+	}
+
+}
