e0517b775e2f515d9b165e967dfc177d72829856
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 13:13:53 +0530

    Added: Push Method in Stack Class

---
+++ src/com/bridgelabz/practiceproblem/stack/StackMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.practiceproblem.stack;
+
+import java.util.Stack;
+
+public class StackMain {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack();
+		
+		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		
+		System.out.println(stack);
+	}
+}
