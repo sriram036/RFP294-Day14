2fd07fb870a7c5b39570a3bfc8eaee764d3fc308
sriram036 <sriramravichandran2608@gmail.com>
2023-08-07 12:23:20 +0530

    Added: Insert Method

--- src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
+++ src/com/bridgelabz/practiceproblem/linkedlist/MainLinkedList.java
@@ -7,8 +7,8 @@ public class MainLinkedList {
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<>();
 		list.addLast(56);
-		list.addLast(30);
 		list.addLast(70);
+		list.add(1, 30);
 		System.out.println(list);
 	}
 
